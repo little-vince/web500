9e134dff2736fcacd23d8452fe6cc8c73aa96b54
nathyong <nathyong@gmail.com>
2014-02-12 21:13:33 +1100

    Added template server code. Does nothing useful now.

---
+++ server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello forest!")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
